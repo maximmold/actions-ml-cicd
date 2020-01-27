159bb47a10d0384178b3ff7402b5d6fc6ec5218a
Maxim Moldenhauer <moldenhauer_maxim@bah.com>
2020-01-27 15:05:22 -0500

    Try to satisfy kubectl command errors

--- kubeflow/train_pipeline/nlp_pipeline.py
+++ kubeflow/train_pipeline/nlp_pipeline.py
@@ -147,18 +147,16 @@ def nlp_pipeline(
 
     deploy_step.after(predict_step)
 
-    # patch_pvc_finalizer = dsl.ContainerOp(
-    #     name="patchpvcfinalizer",
-    #     image="bitnami/kubectl",
-    #     command="kubectl",
-    #     arguments=[
-    #         "--help; kubectl patch `kubectl get pvc -o name -l app=nlp,branch={{workflow.parameters.github-branch}} --field-selector metadata.name!={{workflow.name}}-my-pvc -n kubeflow` -n kubeflow -p",
-    #         "{'metadata':{'finalizers': []}}",
-    #         "--type=merge"
-    #     ]
-    # )
-    #
-    # patch_pvc_finalizer.after(deploy_step)
+    patch_pvc_finalizer = dsl.ContainerOp(
+        name="patchpvcfinalizer",
+        image="bitnami/kubectl",
+        command=["sh", "-c"],
+        arguments=[
+            "kubectl patch `kubectl get pvc -o name -l app=nlp,branch={{workflow.parameters.github-branch}} --field-selector metadata.name!={{workflow.name}}-my-pvc -n kubeflow` -n kubeflow -p {'metadata':{'finalizers': []}} --type=merge"
+        ]
+    )
+
+    patch_pvc_finalizer.after(deploy_step)
 
     delete_previous_pvc = dsl.ContainerOp(
         name="deletepreviouspvc",
@@ -176,7 +174,7 @@ def nlp_pipeline(
         ]
     )
 
-    delete_previous_pvc.after(deploy_step)
+    delete_previous_pvc.after(patch_pvc_finalizer)
 
 
 if __name__ == '__main__':
