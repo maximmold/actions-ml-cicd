a7137b91db6318a200ba6bd9df5e72374dc00d09
Maxim Moldenhauer <moldenhauer_maxim@bah.com>
2020-01-27 15:14:09 -0500

    Try to satisfy kubectl command errors

--- kubeflow/train_pipeline/nlp_pipeline.py
+++ kubeflow/train_pipeline/nlp_pipeline.py
@@ -34,147 +34,147 @@ def nlp_pipeline(
     Pipeline 
     """
 
-    pvc_metadata = V1ObjectMeta(
-        name="{{workflow.name}}-my-pvc",
-        labels={"branch":"{{workflow.parameters.github-branch}}", "app": "nlp"}
-    )
-    requested_resources = V1ResourceRequirements(
-        requests={"storage": "1Gi"}
-    )
-    pvc_spec = V1PersistentVolumeClaimSpec(
-        access_modes=["ReadWriteOnce"],
-        resources=requested_resources,
-        storage_class_name="default"
-    )
-    pvc = V1PersistentVolumeClaim(
-        api_version="v1",
-        kind="PersistentVolumeClaim",
-        metadata=pvc_metadata,
-        spec=pvc_spec
-    )
-
-    vop = dsl.VolumeOp(
-      name="create-pvc",
-      k8s_resource=pvc,
-      modes = None
-    )
-
-
-    download_step = dsl.ContainerOp(
-        name='data_downloader',
-        image='maximmold/data_downloader:0.1',
-        command="python",
-        arguments=[
-            "/microservice/pipeline_step.py",
-            "--labels-path", labels_path,
-            "--features-path", raw_text_path,
-            "--csv-url", csv_url,
-            "--csv-encoding", csv_encoding,
-            "--features-column", features_column,
-            "--labels-column", labels_column
-        ],
-        pvolumes={"/mnt": vop.volume}
-    )
-
-    clean_step = dsl.ContainerOp(
-        name='clean_text',
-        image='maximmold/clean_text_transformer:0.1',
-        command="python",
-        arguments=[
-            "/microservice/pipeline_step.py",
-            "--in-path", raw_text_path,
-            "--out-path", clean_text_path,
-        ],
-        pvolumes={"/mnt": download_step.pvolume}
-    )
-
-    tokenize_step = dsl.ContainerOp(
-        name='tokenize',
-        image='maximmold/spacy_tokenizer:0.1',
-        command="python",
-        arguments=[
-            "/microservice/pipeline_step.py",
-            "--in-path", clean_text_path,
-            "--out-path", spacy_tokens_path,
-        ],
-        pvolumes={"/mnt": clean_step.pvolume}
-    )
-
-    vectorize_step = dsl.ContainerOp(
-        name='vectorize',
-        image='maximmold/tfidf_vectorizer:0.1',
-        command="python",
-        arguments=[
-            "/microservice/pipeline_step.py",
-            "--in-path", spacy_tokens_path,
-            "--out-path", tfidf_vectors_path,
-            "--max-features", tfidf_max_features,
-            "--ngram-range", tfidf_ngram_range,
-            "--action", "train",
-            "--model-path", tfidf_model_path,
-        ],
-        pvolumes={"/mnt": tokenize_step.pvolume}
-    )
-
-    predict_step = dsl.ContainerOp(
-        name='predictor',
-        image='maximmold/lr_text_classifier:0.1',
-        command="python",
-        arguments=[
-            "/microservice/pipeline_step.py",
-            "--in-path", tfidf_vectors_path,
-            "--labels-path", labels_path,
-            "--out-path", lr_prediction_path,
-            "--c-param", lr_c_param,
-            "--action", "train",
-            "--model-path", lr_model_path,
-        ],
-        pvolumes={"/mnt": vectorize_step.pvolume}
-    )
-
-
-    try:
-        seldon_config = yaml.load(open("../deploy_pipeline/seldon_production_pipeline.yaml"))
-    except:
-        # If this file is run from the project core directory 
-        seldon_config = yaml.load(open("deploy_pipeline/seldon_production_pipeline.yaml"))
-
-    deploy_step = dsl.ResourceOp(
-        action="apply",
-        name="seldondeploy",
-        k8s_resource=seldon_config,
-        attribute_outputs={"name": "{.metadata.name}"})
-
-    deploy_step.after(predict_step)
+    # pvc_metadata = V1ObjectMeta(
+    #     name="{{workflow.name}}-my-pvc",
+    #     labels={"branch":"{{workflow.parameters.github-branch}}", "app": "nlp"}
+    # )
+    # requested_resources = V1ResourceRequirements(
+    #     requests={"storage": "1Gi"}
+    # )
+    # pvc_spec = V1PersistentVolumeClaimSpec(
+    #     access_modes=["ReadWriteOnce"],
+    #     resources=requested_resources,
+    #     storage_class_name="default"
+    # )
+    # pvc = V1PersistentVolumeClaim(
+    #     api_version="v1",
+    #     kind="PersistentVolumeClaim",
+    #     metadata=pvc_metadata,
+    #     spec=pvc_spec
+    # )
+    #
+    # vop = dsl.VolumeOp(
+    #   name="create-pvc",
+    #   k8s_resource=pvc,
+    #   modes = None
+    # )
+    #
+    #
+    # download_step = dsl.ContainerOp(
+    #     name='data_downloader',
+    #     image='maximmold/data_downloader:0.1',
+    #     command="python",
+    #     arguments=[
+    #         "/microservice/pipeline_step.py",
+    #         "--labels-path", labels_path,
+    #         "--features-path", raw_text_path,
+    #         "--csv-url", csv_url,
+    #         "--csv-encoding", csv_encoding,
+    #         "--features-column", features_column,
+    #         "--labels-column", labels_column
+    #     ],
+    #     pvolumes={"/mnt": vop.volume}
+    # )
+    #
+    # clean_step = dsl.ContainerOp(
+    #     name='clean_text',
+    #     image='maximmold/clean_text_transformer:0.1',
+    #     command="python",
+    #     arguments=[
+    #         "/microservice/pipeline_step.py",
+    #         "--in-path", raw_text_path,
+    #         "--out-path", clean_text_path,
+    #     ],
+    #     pvolumes={"/mnt": download_step.pvolume}
+    # )
+    #
+    # tokenize_step = dsl.ContainerOp(
+    #     name='tokenize',
+    #     image='maximmold/spacy_tokenizer:0.1',
+    #     command="python",
+    #     arguments=[
+    #         "/microservice/pipeline_step.py",
+    #         "--in-path", clean_text_path,
+    #         "--out-path", spacy_tokens_path,
+    #     ],
+    #     pvolumes={"/mnt": clean_step.pvolume}
+    # )
+    #
+    # vectorize_step = dsl.ContainerOp(
+    #     name='vectorize',
+    #     image='maximmold/tfidf_vectorizer:0.1',
+    #     command="python",
+    #     arguments=[
+    #         "/microservice/pipeline_step.py",
+    #         "--in-path", spacy_tokens_path,
+    #         "--out-path", tfidf_vectors_path,
+    #         "--max-features", tfidf_max_features,
+    #         "--ngram-range", tfidf_ngram_range,
+    #         "--action", "train",
+    #         "--model-path", tfidf_model_path,
+    #     ],
+    #     pvolumes={"/mnt": tokenize_step.pvolume}
+    # )
+    #
+    # predict_step = dsl.ContainerOp(
+    #     name='predictor',
+    #     image='maximmold/lr_text_classifier:0.1',
+    #     command="python",
+    #     arguments=[
+    #         "/microservice/pipeline_step.py",
+    #         "--in-path", tfidf_vectors_path,
+    #         "--labels-path", labels_path,
+    #         "--out-path", lr_prediction_path,
+    #         "--c-param", lr_c_param,
+    #         "--action", "train",
+    #         "--model-path", lr_model_path,
+    #     ],
+    #     pvolumes={"/mnt": vectorize_step.pvolume}
+    # )
+    #
+    #
+    # try:
+    #     seldon_config = yaml.load(open("../deploy_pipeline/seldon_production_pipeline.yaml"))
+    # except:
+    #     # If this file is run from the project core directory
+    #     seldon_config = yaml.load(open("deploy_pipeline/seldon_production_pipeline.yaml"))
+    #
+    # deploy_step = dsl.ResourceOp(
+    #     action="apply",
+    #     name="seldondeploy",
+    #     k8s_resource=seldon_config,
+    #     attribute_outputs={"name": "{.metadata.name}"})
+    #
+    # deploy_step.after(predict_step)
 
     patch_pvc_finalizer = dsl.ContainerOp(
         name="patchpvcfinalizer",
         image="bitnami/kubectl",
         command=["sh", "-c"],
         arguments=[
-            "kubectl patch `kubectl get pvc -o name -l app=nlp,branch={{workflow.parameters.github-branch}} --field-selector metadata.name!={{workflow.name}}-my-pvc -n kubeflow` -n kubeflow -p {'metadata':{'finalizers': []}} --type=merge"
+            "set -x; for j in $(kubectl get pvc -o name -l app=nlp,branch={{workflow.parameters.github-branch}} --field-selector metadata.name!={{workflow.name}}-my-pvc -n kubeflow) do kubectl patch -n kubeflow $j -p {'metadata':{'finalizers': []}} --type=merge done"
         ]
     )
 
-    patch_pvc_finalizer.after(deploy_step)
-
-    delete_previous_pvc = dsl.ContainerOp(
-        name="deletepreviouspvc",
-        image="bitnami/kubectl",
-        command="kubectl",
-        arguments=[
-            "delete",
-            "pvc",
-            "-l",
-            "app=nlp,branch={{workflow.parameters.github-branch}}",
-            "--field-selector",
-            "metadata.name!={{workflow.name}}-my-pvc",
-            "--grace-period=0",
-            "--force"
-        ]
-    )
-
-    delete_previous_pvc.after(patch_pvc_finalizer)
+    # patch_pvc_finalizer.after(deploy_step)
+    #
+    # delete_previous_pvc = dsl.ContainerOp(
+    #     name="deletepreviouspvc",
+    #     image="bitnami/kubectl",
+    #     command="kubectl",
+    #     arguments=[
+    #         "delete",
+    #         "pvc",
+    #         "-l",
+    #         "app=nlp,branch={{workflow.parameters.github-branch}}",
+    #         "--field-selector",
+    #         "metadata.name!={{workflow.name}}-my-pvc",
+    #         "--grace-period=0",
+    #         "--force"
+    #     ]
+    # )
+    #
+    # delete_previous_pvc.after(patch_pvc_finalizer)
 
 
 if __name__ == '__main__':
